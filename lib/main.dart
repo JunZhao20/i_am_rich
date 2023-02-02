25edf5ba155266c5a4c8d6f5ffde04161333042a
Jun Zhao <junzhao1111@gmail.com>
2023-02-02 18:11:18 +0000

    Centered image

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey[400],
-        body: const Image(
-          image: NetworkImage(
-              'https://media.istockphoto.com/id/135522870/photo/millennium-bridge-in-gateshead.jpg?s=612x612&w=0&k=20&c=zg4m31hHmCuN1GWecsd0S-5DObWm2HecGv5jEEsnCGU='),
+        body: Center(
+          child: const Image(
+            image: NetworkImage(
+                'https://media.istockphoto.com/id/135522870/photo/millennium-bridge-in-gateshead.jpg?s=612x612&w=0&k=20&c=zg4m31hHmCuN1GWecsd0S-5DObWm2HecGv5jEEsnCGU='),
+          ),
         ),
         appBar: AppBar(
           backgroundColor: Colors.blueGrey[900],

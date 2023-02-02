1b477c6c14a7087226a162ee369317eebdaad973
Jun Zhao <junzhao1111@gmail.com>
2023-02-02 18:02:09 +0000

    Added scaffold with appbar including color modification and image.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[400],
+        body: const Image(
+          image: NetworkImage(
+              'https://media.istockphoto.com/id/135522870/photo/millennium-bridge-in-gateshead.jpg?s=612x612&w=0&k=20&c=zg4m31hHmCuN1GWecsd0S-5DObWm2HecGv5jEEsnCGU='),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text('I Am Rich'),
+        ),
       ),
     ),
   );
